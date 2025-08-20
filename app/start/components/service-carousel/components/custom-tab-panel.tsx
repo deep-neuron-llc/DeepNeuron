6d9121e6803e4d45e397cf63d0cc3d4b42ac26d4
Shoeb Joarder <shoeb.joarder@uni-due.de>
2025-08-20 23:11:37 +0200

    Add Lottie component in service component

--- app/start/components/service-carousel/components/custom-tab-panel.tsx
+++ app/start/components/service-carousel/components/custom-tab-panel.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import Lottie from "lottie-react";
 
 interface TabPanelProps {
   index: number;
   value: number;
   label: string;
   description: string;
+  image: object;
 }
 
 const CustomTabPanel = ({
@@ -13,25 +15,29 @@ const CustomTabPanel = ({
   index,
   label,
   description,
+  image,
 }: TabPanelProps) => {
   return (
     <div id={`full-width-tabpanel-${index}`} hidden={value !== index}>
       {value === index && (
         <Box sx={{ px: 2, py: 5 }}>
           <Grid container spacing={4} alignItems="center">
-            <Grid size={{ xs: 12, md: 6 }} >
-              <Typography variant="h4" gutterBottom>
+            <Grid size={{ xs: 12, md: 6 }}>
+              <Typography variant="h5" gutterBottom>
                 {label}
               </Typography>
               <Typography
-                variant="h5"
+                variant="h6"
                 dangerouslySetInnerHTML={{ __html: description }}
               />
             </Grid>
-            <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "grid" } }}>
-              <Paper variant="outlined" sx={{ height: 400, p: 3 }}>
-                <Typography align="center">Image to be provided</Typography>
-              </Paper>
+            <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "grid" } }}>
+              <Lottie
+                animationData={image}
+                loop={true}
+                autoplay={true}
+                style={{ height: 400 }}
+              />
             </Grid>
           </Grid>
         </Box>
