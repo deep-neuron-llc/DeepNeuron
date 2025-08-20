0418d5703cf22f72f3f09f1e5381644dfea9aa30
kazi <kaziibrahim.khalil@rtl.de>
2025-08-20 22:32:28 +0200

    chore: added new lines in e2e test files

--- e2e-test/home.cy.ts
+++ e2e-test/home.cy.ts
@@ -1,14 +1,14 @@
 describe("Homepage", () => {
-    it("clicks the 'Book an appointment' CTA and navigates to contact", () => {
-      cy.visit("/");
-  
-      cy.contains('a, [role="button"]', /^Book an appointment$/i, { timeout: 10000 })
-        .should('be.visible')
-        .scrollIntoView({ offset: { top: -80, left: 0 } })
-        .click();
-  
-      cy.location('pathname', { timeout: 10000 })
-        .should('match', /\/contact(-us)?$/);
-    });
+  it("clicks the 'Book an appointment' CTA and navigates to contact", () => {
+    cy.visit("/");
+
+    cy.contains('a, [role="button"]', /^Book an appointment$/i, { timeout: 10000 })
+      .should('be.visible')
+      .scrollIntoView({ offset: { top: -80, left: 0 } })
+      .click();
+
+    cy.location('pathname', { timeout: 10000 })
+      .should('match', /\/contact(-us)?$/);
+  });
 });
-  
+
